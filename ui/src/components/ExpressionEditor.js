773f50cae44d837a1a685f91a9724c9f10e3f669
Dennis Dunn <ansofive@gmail.com>
2019-04-02 21:53:30 -0400

    Minor UI change.

--- ui/src/components/ExpressionEditor.js
+++ ui/src/components/ExpressionEditor.js
@@ -1,16 +1,12 @@
-import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Icon from 'react-fontawesome';
+import React from 'react';
 
 const styles = {
     root: {
         flexGrow: 1
-    },
-    header: {
-        textAlign: 'center',
-        paddingBottom: 10
     }
 }
 
@@ -47,13 +43,18 @@ export class ExpressionEditor extends React.Component {
         this.setState({ items });
     }
 
+    isValid() {
+        const { items } = this.state;
+        return items.length === 0 ? false : items.map(x => x.length).reduce((t, c) => t * c, 1);
+    }
+
     render() {
         const { items } = this.state;
         const controls = items.map((x, idx) =>
             <Form.Group key={idx}>
                 <Form.Row>
                     <Col>
-                        <Form.Control defaultValue={x} onChange={this.mkOnChange(idx).bind(this)} size='sm' />
+                        <Form.Control defaultValue={x} onChange={this.mkOnChange(idx).bind(this)} size='sm' placeholder='f(x)'/>
                     </Col>
                     <Col xs={2}>
                         <Button variant="secondary" size="sm" onClick={this.mkOnRemove(idx).bind(this)}><Icon name="minus" /></Button>
@@ -64,9 +65,6 @@ export class ExpressionEditor extends React.Component {
         return (
             <div style={styles.root}>
                 <Form>
-                    <h3 style={styles.header}>
-                        f(x)
-                    </h3>
                     {controls}
                     <Form.Group>
                         <Form.Row>
@@ -77,7 +75,7 @@ export class ExpressionEditor extends React.Component {
                             </Col>
                         </Form.Row>
                     </Form.Group>
-                    <Button variant="primary" onClick={this.apply.bind(this)}>Apply</Button>
+                    <Button variant="primary" onClick={this.apply.bind(this)} disabled={!this.isValid()}>Apply</Button>
                 </Form>
             </div>
         );
